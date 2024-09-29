b1cc4e9e56a82bcfed6f7176a731c83b271d84e1
Dimitrina Nestorova <136362960+anirdm@users.noreply.github.com>
2024-09-29 12:13:05 +0300

    restructure: separate frontend and backend into distinct folders

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className=' flex justify-center bg-stone-700 p-3 border-t border-t-stone-400'>
+    <footer className=' flex justify-center bg-black p-3 border-t border-t-stone-400'>
       <span className='text-xs text-gray-200'>© 2024 MyRecipeBook by Dimi </span>
     </footer>
   )
